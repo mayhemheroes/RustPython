696f273aa9981bd168427af3504cdfb26bdb1a93
Dylan <57853417+4812571@users.noreply.github.com>
2023-04-17 11:44:53 -0400

    use arbitrary

--- compiler/parser/fuzz/fuzz_targets/fuzz_parse_program.rs
+++ compiler/parser/fuzz/fuzz_targets/fuzz_parse_program.rs
@@ -1,62 +1,20 @@
 #![no_main]
-struct FuzzedDataProvider {
-    data: Vec<u8>,
-    index: usize,
-}
-
-impl FuzzedDataProvider {
-    fn new(data: &[u8]) -> Self {
-        Self {
-            data: data.to_vec(),
-            index: 0,
-        }
-    }
-
-    fn remaining_len(&self) -> usize {
-        self.data.len() - self.index
-    }
-
-    fn consume_i32(&mut self) -> Option<i32> {
-        if self.remaining_len() < 4 {
-            return None;
-        }
-
-        let mut buf = [0u8; 4];
-        buf.copy_from_slice(&self.data[self.index..self.index + 4]);
-        self.index += 4;
-        Some(i32::from_le_bytes(buf))
-    }
-
-    fn consume_string(&mut self, len: usize) -> Option<String> {
-        if self.remaining_len() < len {
-            return None;
-        }
-
-        let s = String::from_utf8(self.data[self.index..self.index + len].to_vec()).ok()?;
-        self.index += len;
-        Some(s)
-    }
-}
 
 use libfuzzer_sys::fuzz_target;
 use rustpython_parser::parse_program;
 
+// use arbitrary to create two strings from the fuzzed data
+use libfuzzer_sys::arbitrary::Arbitrary;
+use libfuzzer_sys::arbitrary::Unstructured;
+
 fuzz_target!(|data: &[u8]| {
-    let mut data_provider = FuzzedDataProvider::new(data);
+    // generate unstructured
+    let mut unstructured = Unstructured::new(data);
 
-    let str_1_len = data_provider.consume_i32().unwrap_or(0) as usize;
-    let str_2_len = data_provider.consume_i32().unwrap_or(0) as usize;
-
-    let str_1 = data_provider.consume_string(str_1_len).unwrap_or(String::new());
-    let str_2 = data_provider.consume_string(str_2_len).unwrap_or(String::new());
-
-    if !str_1.is_empty() {
-        println!("str_1: {}", str_1);
-    }
-
-    if !str_2.is_empty() {
-        println!("str_2: {}", str_2);
-    }
-
+    // generate strings
+    let str_1 = String::arbitrary(&mut unstructured).unwrap_or(String::new());
+    let str_2 = String::arbitrary(&mut unstructured).unwrap_or(String::new());
+    
+    // parse program
     let _ = parse_program(&str_1, &str_2);
 });
