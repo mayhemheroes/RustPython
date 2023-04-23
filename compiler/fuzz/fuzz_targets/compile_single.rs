58a2c116d77b299501011e90a777e0b463217df4
Dylan <57853417+4812571@users.noreply.github.com>
2023-04-23 10:13:43 -0400

    new fuzzers

---
+++ compiler/fuzz/fuzz_targets/compile_single.rs
@@ -0,0 +1,15 @@
+#![no_main]
+
+use rustpython_compiler::Mode;
+use rustpython_compiler::CompileOpts;
+use rustpython_compiler::compile;
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    // generate source
+    let source = String::from_utf8_lossy(data).to_string();
+    
+    // parse program
+    let _ = compile(&source, Mode::Single, String::from("<fuzz>"), CompileOpts::default());
+});
